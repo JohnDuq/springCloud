925759c9a693d04a5a23109fd06a7df4e0d1d0e2
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-01-12 10:32:48 -0500

    Update ItemController.java

--- service-producer/src/main/java/com/udemy/spring/cloud/service/producer/app/controller/ItemController.java
+++ service-producer/src/main/java/com/udemy/spring/cloud/service/producer/app/controller/ItemController.java
@@ -8,12 +8,11 @@ import com.udemy.spring.cloud.service.producer.app.service.IItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ItemController {
-    
+
     @Autowired
     private IItemService iItemService;
 
