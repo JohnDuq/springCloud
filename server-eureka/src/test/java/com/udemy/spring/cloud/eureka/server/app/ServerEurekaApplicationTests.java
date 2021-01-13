de9e955da7fae616367399b1ca1ea5d33f641834
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-01-13 15:19:36 -0500

    Proyecto eureka
    Proyecto eureka

---
+++ server-eureka/src/test/java/com/udemy/spring/cloud/eureka/server/app/ServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.cloud.eureka.server.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
