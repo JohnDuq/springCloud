110d75c1a4ca1638a7bc239b5cab831664fe0cb3
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-01-13 12:51:20 -0500

    Balanceo de carga con Ribbon y Configuraciones Visual Studio

--- service-client/src/main/java/com/udemy/spring/cloud/service/item/app/client/IServiceProducerFeignClient.java
+++ service-client/src/main/java/com/udemy/spring/cloud/service/item/app/client/IServiceProducerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "service-producer", url = "localhost:8001")
+@FeignClient(name = "service-producer")
 public interface IServiceProducerFeignClient {
 
     @GetMapping("/findAll")
