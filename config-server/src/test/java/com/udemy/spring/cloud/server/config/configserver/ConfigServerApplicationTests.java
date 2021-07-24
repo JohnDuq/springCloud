7a9f1a4cca32b1c6e5fe54656bfe489b7e03101e
Jhonnatan Duque <Jhonnatan.DuqueRamos@endava.com>
2021-07-24 18:58:04 -0500

    Servidor de configuración y archivo de configuración

---
+++ config-server/src/test/java/com/udemy/spring/cloud/server/config/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.cloud.server.config.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
