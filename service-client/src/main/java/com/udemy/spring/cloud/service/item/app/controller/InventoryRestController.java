1c61f6c1895f8a10097dd91b6a350d519863d3b0
Jhonnatan Duque <Jhonnatan.DuqueRamos@endava.com>
2021-07-27 17:14:54 -0500

    Crud completo en Feign client e implementación directa 2

--- service-client/src/main/java/com/udemy/spring/cloud/service/item/app/controller/InventoryRestController.java
+++ service-client/src/main/java/com/udemy/spring/cloud/service/item/app/controller/InventoryRestController.java
@@ -12,7 +12,7 @@ import com.udemy.spring.cloud.service.item.app.service.IInventoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
