c45b60ff2fe2b3c1dd4ab31c2967b4282f6896f1
Jhonnatan Duque <Jhonnatan.DuqueRamos@endava.com>
2021-08-03 11:18:37 -0500

    Metodo para exponer el id de las clases en los json de respuesta de los @RepositoryRestResource

---
+++ user-cloud/src/main/java/com/udemy/spring/cloud/user/RepositoryConfig.java
@@ -0,0 +1,22 @@
+package com.udemy.spring.cloud.user;
+
+import com.udemy.spring.cloud.user.model.data.Role;
+import com.udemy.spring.cloud.user.model.data.User;
+import com.udemy.spring.cloud.user.model.data.UserRole;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+    // Metodo para exponer el id de las clases en los json de respuesta de los
+    // @RepositoryRestResource
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(User.class, Role.class, UserRole.class);
+    }
+
+}
