dc3526932f076201b6cdee88eeec73378ba09cc3
jhonnatanduque <jhonnatan.duque@paysafe.com>
2021-08-25 13:20:43 -0500

    ajuste de librería

--- service-email/src/main/java/com/udemy/spring/cloud/email/model/data/ConfirmationToken.java
+++ service-email/src/main/java/com/udemy/spring/cloud/email/model/data/ConfirmationToken.java
@@ -14,8 +14,6 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.apache.commons.lang3.RandomStringUtils;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
