174d681deca2bcef9cc17cec44860ecf7ef054ca
jhonnatanduque <jhonnatan.duque@paysafe.com>
2021-08-25 14:47:06 -0500

    Desacople de información de seguridad en servidor de configuracion

--- service-auth/src/main/java/com/udemy/spring/cloud/oauth/security/AuthorizationServerConfig.java
+++ service-auth/src/main/java/com/udemy/spring/cloud/oauth/security/AuthorizationServerConfig.java
@@ -3,8 +3,10 @@ package com.udemy.spring.cloud.oauth.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -15,10 +17,13 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
+@RefreshScope
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -41,8 +46,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("consumerApp") // Definen los consumidores
-                .secret(bCryptPasswordEncoder.encode("54321")) // Define el secreto de autenticacion para el consumidor
+                .withClient(env.getProperty("config.security.oauth.client.id")) // Definen los consumidores
+                .secret(bCryptPasswordEncoder.encode(env.getProperty("config.security.oauth.client.secret"))) // Define el secreto de autenticacion para el consumidor
                 .scopes("read", "write") // Define el alcance del consumidor
                 .authorizedGrantTypes("password", "refresh_token") // Define el metodo de autenticacion de los usuarios
                                                                    // en el consumidor, refresh token renueva el token
@@ -76,7 +81,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("algun_codigo_secreto_aeiou"); // Define secreto de codificacion del JWT
+        jwtAccessTokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key")); // Define secreto de codificacion del JWT
         return jwtAccessTokenConverter;
     }
 
--- zuul-gateway/src/main/java/com/udemy/spring/cloud/gateway/zuul/zuulgateway/auth/ResourceServerConfig.java
+++ zuul-gateway/src/main/java/com/udemy/spring/cloud/gateway/zuul/zuulgateway/auth/ResourceServerConfig.java
@@ -1,5 +1,7 @@
 package com.udemy.spring.cloud.gateway.zuul.zuulgateway.auth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,9 +12,13 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${config.security.oauth.jwt.key}")
+    private String secretKeyJWT;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -64,7 +70,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
-        jwtAccessTokenConverter.setSigningKey("algun_codigo_secreto_aeiou"); // Define secreto de codificacion del JWT
+        jwtAccessTokenConverter.setSigningKey(secretKeyJWT); // Define secreto de codificacion del JWT
         return jwtAccessTokenConverter;
     }
 
