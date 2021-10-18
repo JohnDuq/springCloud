57b233db55a2a4b720c0d43ca79e5332957c74a7
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-10-18 11:06:32 -0500

    Filtros para las rutas

--- springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
+++ springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.formacionbdi.springboot.app.gateway.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -12,6 +13,18 @@ public class SpringSecurityConfig {
 	public SecurityWebFilterChain configure(ServerHttpSecurity serverHttpSecurity) {
 		return serverHttpSecurity.authorizeExchange()
 				.pathMatchers("/gateway/api/security/**").permitAll()
+				.pathMatchers(HttpMethod.GET, 
+								"/gateway/api/producer/findAll",
+								"/gateway/api/producer/findById/**",
+								"/gateway/api/client/findAll",
+								"/gateway/api/client/findByIdAmount/**",
+								"/gateway/api/user/user-dao",
+								"/gateway/api/user/user-dao/search/findByUsername",
+								"/gateway/api/user/role-dao/search/getRolesByUser").permitAll()
+				.pathMatchers(HttpMethod.GET, "/gateway/api/user/user-dao/search/find-id").hasAnyRole("ADMIN", "USER")
+				.pathMatchers("/gateway/api/producer/**", 
+								"/gateway/api/client/**", 
+								"/gateway/api/user/**").hasRole("ADMIN")
 				.anyExchange().authenticated()
 				.and()
 				.csrf().disable()
