e6731d016676098b4dd42257a50fe3f6b4bff979
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-10-29 21:06:37 -0500

    Ajuste para conexión de microservicios con eureka y congig server en docker

--- service-producer/src/test/java/com/udemy/spring/cloud/service/producer/app/ServiceProducerApplicationTests.java
+++ service-producer/src/test/java/com/udemy/spring/cloud/service/producer/app/ServiceProducerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.udemy.spring.cloud.service.producer.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceProducerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
