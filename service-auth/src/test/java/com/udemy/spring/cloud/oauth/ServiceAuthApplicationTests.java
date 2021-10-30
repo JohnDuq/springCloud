7953538eee4013f38b2fca8edcfdf087e4cf9056
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-10-30 09:51:11 -0500

    Servicio auth en docker

--- service-auth/src/test/java/com/udemy/spring/cloud/oauth/ServiceAuthApplicationTests.java
+++ service-auth/src/test/java/com/udemy/spring/cloud/oauth/ServiceAuthApplicationTests.java
@@ -1,13 +1,8 @@
 package com.udemy.spring.cloud.oauth;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceAuthApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
