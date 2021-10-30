01528c12d1e3e4d124f7d63fddaa6993de03c4f6
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-10-30 08:58:19 -0500

    Docker para zuul y user-role

--- zuul-gateway/src/test/java/com/udemy/spring/cloud/gateway/zuul/zuulgateway/ZuulGatewayApplicationTests.java
+++ zuul-gateway/src/test/java/com/udemy/spring/cloud/gateway/zuul/zuulgateway/ZuulGatewayApplicationTests.java
@@ -1,13 +1,8 @@
 package com.udemy.spring.cloud.gateway.zuul.zuulgateway;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ZuulGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
