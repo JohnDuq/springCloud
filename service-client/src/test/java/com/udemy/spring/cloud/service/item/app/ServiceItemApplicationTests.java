83781eaa791fc6f5faad40275f332996a5e6a143
JohnDuq <jonathan.duque.ramos@gmail.com>
2021-10-30 10:00:49 -0500

    cliente docker

--- service-client/src/test/java/com/udemy/spring/cloud/service/item/app/ServiceItemApplicationTests.java
+++ service-client/src/test/java/com/udemy/spring/cloud/service/item/app/ServiceItemApplicationTests.java
@@ -1,13 +1,8 @@
 package com.udemy.spring.cloud.service.item.app;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceItemApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
