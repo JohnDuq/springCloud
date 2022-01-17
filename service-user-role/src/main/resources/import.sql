6ba5d48b818586069e847999a80430f24d3b592c
JohnDuq <jonathan.duque.ramos@gmail.com>
2022-01-17 10:50:31 -0500

    ajuste user role

--- service-user-role/src/main/resources/import.sql
+++ service-user-role/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO user (username, password, status, name, last_name, email, create_for, create_at, update_for, update_at) VALUES ('ROOT', '$2a$10$G4YNGGczQirzq4uiOP4cieU.cl/qNjFjUn2jKYtMDLU94eeVh8Cea', 'ENABLE', 'ROOT', 'ROOT', 'ROOT@EMAIL.COM', 'ROOT', NOW(), NULL, NULL);
-INSERT INTO user (username, password, status, name, last_name, email, create_for, create_at, update_for, update_at) VALUES ('USER', '$2a$10$rPdOAeTZe7fhuvx5t7Go/Oqbud06Ug8tyQiJhPQO27fr6PyVFafGW', 'ENABLE', 'USER', 'USER', 'USER@EMAIL.COM', 'ROOT', NOW(), NULL, NULL);
+INSERT INTO user (username, password, status, name, last_name, email, emailStatus, create_for, create_at, update_for, update_at) VALUES ('ROOT', '$2a$10$G4YNGGczQirzq4uiOP4cieU.cl/qNjFjUn2jKYtMDLU94eeVh8Cea', 'ENABLE', 'ROOT', 'ROOT', 'ROOT@EMAIL.COM', 'VERIFIED', 'ROOT', NOW(), NULL, NULL);
+INSERT INTO user (username, password, status, name, last_name, email, emailStatus, create_for, create_at, update_for, update_at) VALUES ('USER', '$2a$10$rPdOAeTZe7fhuvx5t7Go/Oqbud06Ug8tyQiJhPQO27fr6PyVFafGW', 'ENABLE', 'USER', 'USER', 'USER@EMAIL.COM', 'VERIFIED', 'ROOT', NOW(), NULL, NULL);
 
 INSERT INTO role (name, create_for, create_at) VALUES ('ROLE_ADMIN', 'ROOT', NOW());
 INSERT INTO role (name, create_for, create_at) VALUES ('ROLE_USER', 'ROOT', NOW());
